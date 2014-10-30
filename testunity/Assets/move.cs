5afa909cdc9d80072695f8df35923e241b3d41a8
hayahsi <70yolu@gmail.com>
2014-10-30 13:54:41 +0900

    no message

---
+++ testunity/Assets/move.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+
+	float rotetx = 0;
+	float rotetz = 0;
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			transform.Rotate(1, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(0, 0, 1);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Rotate(-1, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(0, 0, -1);
+		}
+	}
+}
